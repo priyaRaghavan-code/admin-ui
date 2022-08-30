4690337b3736c7dae0c6ed4a24dc5cdc025e3724
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2022-08-30 14:40:19 +0530

    Added Search and delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { SearchBar } from "./lib/components/SearchBar";
 import { Table } from "./lib/components/Table";
 import { mockResponseData } from "./lib/utils/data";
 
 function App() {
+  let [responseData, setResponseData] = useState(mockResponseData);
+
+  const searchOnChange = (value) => {
+    const filter_ = (value) => {
+      return mockResponseData.filter((e) => {
+        const val = value.toLowerCase();
+        return (
+          e.name.toLowerCase().includes(val) ||
+          e.role.toLowerCase().includes(val) ||
+          e.email.toLowerCase().includes(val)
+        );
+      });
+    };
+
+    setResponseData(filter_(value));
+  };
+
+  const deleteRow = (id) => {
+    setResponseData(responseData.filter((e) => e.id !== id));
+  };
+
   return (
     <main className="flex flex-col items-center max-w-6xl mx-auto m-4 gap-4">
-      <SearchBar />
-      <Table rows={mockResponseData} />
+      <SearchBar onChangeCallBack={searchOnChange} />
+      {responseData.length > 0 ? (
+        <Table rows={responseData} onDelete={deleteRow} />
+      ) : (
+        <p className="text-md text-red-600">No results found for the Query</p>
+      )}
     </main>
   );
 }
--- src/lib/components/SearchBar/index.jsx
+++ src/lib/components/SearchBar/index.jsx
@@ -1,6 +1,15 @@
-export function SearchBar() {
+import { useEffect, useState } from "react";
+
+export function SearchBar({ onChangeCallBack }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    onChangeCallBack(searchQuery);
+  }, [searchQuery]);
+
   return (
     <input
+      onChange={(e) => setSearchQuery(e.target.value)}
       type="text"
       placeholder="Search by name, email or role"
       className="w-full rounded-none border-gray-300 border p-2 focus:outline-none focus:border-gray-600"
